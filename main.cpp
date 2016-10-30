f108855ad06f9fec9713e03a8cb55b2bc28153e9
kama <ksydykbekov@gmail.com>
2016-10-30 16:20:37 +0100

    Показал измения в одной точке

--- main.cpp
+++ main.cpp
@@ -17,21 +17,26 @@ int main()
 
     int r1, s1, r2, s2;
 
-    r1 = 100;
+    r1 = 90;
     s1 = 150;
-    r2 = 200;
-    s2 = 250;
+    r2 = 150;
+    s2 = 255;
 
  /*   cout<<"Enter r1: "<<endl; cin>>r1;
     cout<<"Enter s1: "<<endl; cin>>s1;
     cout<<"Enter r2: "<<endl; cin>>r2;
     cout<<"Enter s2: "<<endl; cin>>s2;*/
 
+
+
     for(int y = 0; y < image.rows; y++){
         for(int x = 0; x < image.cols; x++){
             for(int c = 0; c < 3; c++){
                 int output = computeOutput(image.at<Vec3b>(y,x)[c], r1, s1, r2, s2);
                 new_image.at<Vec3b>(y,x)[c] = saturate_cast<uchar>(output);
+                if (y == 1 && x ==1 &&  c ==0)     cout<<"\nRed channel  => "<< "x =  "<<(int)image.at<Vec3b>(y,x)[c] <<" output =  " << output;
+                if (y == 1 && x ==1 &&  c ==1)     cout<<"\nGreen channel=> "<< "x =  "<<(int)image.at<Vec3b>(y,x)[c] <<" output =  " << output;
+                if (y == 1 && x ==1 &&  c ==2)     cout<<"\nBlue channel => "<< "x =  "<<(int)image.at<Vec3b>(y,x)[c] <<" output =  " << output;
             }
         }
     }
@@ -49,6 +54,8 @@ int main()
 
 int computeOutput(int x, int r1, int s1, int r2, int s2)
 {
+
+
     float result;
     if(0 <= x && x <= r1){
         result = s1/r1 * x;
